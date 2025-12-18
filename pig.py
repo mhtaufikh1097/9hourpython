32be8e7ab64d1a7c6f12ae95ac83d99d11642684
Muhammad Taufik Hidayat <muhammad.hidayat@KCIC.net>
2025-12-18 08:41:44 +0700

    random

--- pig.py
+++ pig.py
@@ -20,4 +20,30 @@ while True:
     else:
         print("Try again gais.")
         
-print(players)
+max_score = 50
+players_scores = [0 for _ in range(players)]
+
+while max(players_scores) < max_score:
+    for player_idx in range(players):
+        print("\nPlayer", player_idx + 1, "turn has just started!\n")
+        current_score = 0
+
+
+    while True:
+        should_roll = input("Would you like to roll (y) ? ")
+        if should_roll.lower() != "y":
+            break
+        
+        value = roll()
+        if value == 1:
+            print("You rolled a 1! Turn done!")
+            current_score = 0
+            break
+        else:
+            current_score += value
+            print("You rolled a:", value)
+        
+        print("Your score is:", current_score)
+    
+    players_scores[player_idx] += current_score
+    print("your total score is:", players_scores[player_idx])
---
+++ random.py
@@ -0,0 +1,17 @@
+# kecepatan  = int(input("Please input kecepatan = "))
+# jarak = int(input("Please input jarak = "))
+# waktu = int(input("Please input waktu = "))
+
+# waktu_jam  = jarak / kecepatan
+# waktu_menit = waktu_jam * 60
+
+# print(f"Kecepatan adalah {kecepatan} km/h" )
+# print(f"Jarak adalah {jarak} km")
+
+# print(f"Waktu adalah {waktu_menit:.2f} menit")
+
+#                                                       ======Looping
+i = 5
+while i > 0:
+    print(i)
+    i -= 1
